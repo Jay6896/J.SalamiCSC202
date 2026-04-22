49af4f2bd7cdadcf2f1169f41acf0e4dc64f6175
Jay6896 <jamilsalami08@gmail.com>
2026-04-22 21:03:00 +0100

    Week - 6

---
+++ Week - 6/Typescript_Intro.js
@@ -0,0 +1,13 @@
+"use strict";
+let value = 12;
+let namez = "John";
+function nameAge(myname, myval) {
+    console.log(`My name is ${myname} and I am ${myval}`);
+}
+nameAge("John", 20);
+let arr1 = [1, 2, 3, 4];
+console.log(arr1.slice(1));
+class Myclass {
+    constructor(val) {
+    }
+}
---
+++ Week - 6/Typescript_Intro.ts
@@ -0,0 +1,19 @@
+let value:number = 12;
+let namez:string = "John";
+  
+function nameAge(myname:string, myval:number){
+    console.log(`My name is ${myname} and I am ${myval}`)
+}
+
+nameAge("John", 20);
+
+let arr1:Array<number> = [1,2,3,4];
+
+console.log(arr1.slice(1));
+
+class Myclass
+{
+    constructor(val:string){
+
+    }
+}
